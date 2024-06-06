2e354aeefe1b37878dc85f15a229a36912936b1e
Joseph Karl Crisostomo <josephkarl.crisostomo@asti.dost.gov.ph>
2024-06-06 11:00:39 +0800

    Add tests for the information endpoint

--- core/tests.py
+++ core/tests.py
@@ -92,3 +92,22 @@ class InferenceEndpointTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn('model_artifacts', response.data)
 
+
+
+
+class InformationEndpointTest(APITestCase):
+     
+
+    def test_information_endpoint_request(self):
+        response = self.client.get('/info/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_information_endpoint_contains_expected_fields(self):
+        response = self.client.get('/info/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.headers.get('Content-Type'), 'application/json')
+        response_data = response.json()
+        self.assertIn('input_files', response_data)
+        self.assertIn('model_artifacts', response_data)
+        
+     
