b5be1547e08da349fa205c0d0b5be5d299985be1
JosephCrisostomo <joseph.crisostomo@dashlabs.ai>
2024-06-19 12:43:11 +0800

    Configure CORS

--- app/settings.py
+++ app/settings.py
@@ -53,7 +53,9 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
+    "http://localhost:5173",
     "http://127.0.0.1:5174",
+    "http://localhost:5174",
 ]
 
 ROOT_URLCONF = 'app.urls'
